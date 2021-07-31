50f8eef05486801d219a52de92b2449a15535e2f
Mohamed Nabassoua <mnabassoua@gmail.com>
2021-07-31 19:11:21 -0400

    Updates

---
+++ Basics/intro.py
@@ -0,0 +1,9 @@
+# Python basics
+
+text_var = "Hello Mo!"
+int_var = 40
+float_var = 3.14
+
+print(type(text_var))
+print(type(int_var))
+print(type(float_var))
---
+++ Intro/Var101.py
@@ -0,0 +1,17 @@
+# Python variable types
+
+text_var = "Hello Mo!"
+int_var = 40
+float_var = 3.14
+
+print(type(text_var))
+print(type(int_var))
+print(type(float_var))
+
+# Affectation on the same line
+
+text_var2, int_var2, float_var2 = ("Me again", 41, 6.089)
+
+print(type(text_var2))
+print(type(int_var2))
+print(type(float_var2))
