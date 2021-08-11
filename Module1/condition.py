d2d47676af4723d382ec7c9e9bc6a3031209f67c
Mohamed Nabassoua <mnabassoua@gmail.com>
2021-08-11 18:34:32 -0400

    Update

---
+++ Module1/condition.py
@@ -0,0 +1,12 @@
+# -*- coding: latin-1 -*-
+"""
+Created on Tue Apr 12 10:45:34 2016
+
+@author: chmartin
+"""
+
+a = 20
+if a > 100:
+	print("a dépasse la centaine")
+else:
+	print("nous sommes sortis de la condition")
