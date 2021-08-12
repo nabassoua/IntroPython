092b0d939635a07499e870703cf11737a5107350
Mohamed Nabassoua <mnabassoua@gmail.com>
2021-08-11 22:18:52 -0400

    Update classe_3.py

--- Module1/classe_3.py
+++ Module1/classe_3.py
@@ -9,20 +9,19 @@ b = input("Please enter the second number: ")
 c = input("Please enter the third number: ")
 
 # Need to check if the user entered numbers...
-if (type(a) != int and type(a) != float) or (type(b) != int and type(b) != float) or (type(c) != int and type(c) != float):
+if ((type(a) != int and type(a) != float) or (type(b) != int and type(b) != float) or (type(c) != int and type(c) != float)):
     print("Try again, please enter numbers")
-
-if a <= b and b <= c:
-    print(a, b, c)
-elif b <= a and a <= c:
-    print(b, a, c)
-elif c <= a and a <= b:
-    print(c, a, b)
-elif c <= b and b <= a:
-    print(c, b, a)
-elif a <= c and c <= b:
-    print(a, c, b)
-elif b <= c and c <= a:
-    print(b, c, a)
 else:
-    print("More work needed....")
+    if a <= b and b <= c:
+        print(a, b, c)
+    elif b <= a and a <= c:
+        print(b, a, c)
+    elif c <= a and a <= b:
+        print(c, a, b)
+    elif c <= b and b <= a:
+        print(c, b, a)
+    elif a <= c and c <= b:
+        print(a, c, b)
+    elif b <= c and c <= a:
+        print(b, c, a)
+
