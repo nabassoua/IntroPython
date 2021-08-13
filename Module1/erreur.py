2f3846ab941c305aad2b14265f27959bd0652c14
Mohamed Nabassoua <mnabassoua@gmail.com>
2021-08-13 14:42:43 -0400

    update

---
+++ Module1/erreur.py
@@ -0,0 +1,35 @@
+# -*- coding: latin-1 -*-
+
+
+def somme1(x, y) :
+    """
+    x : int
+    y : int
+    ->  int
+    """
+    z = x + y
+    return z
+
+def somme2(x, t) :
+    """
+    x : int
+    t : int
+    ->  int
+    """
+    z = x + t
+    return z
+
+def somme3(x, y) :
+    """
+    x : int
+    y : int
+    ->  int    
+    """
+    z = x + y
+    return z
+
+
+
+print('Somme1 : ', somme1(10, 15))
+print('Somme2 : ', somme2(12, 17))
+print('Somme3 : ', somme3(16, 15))
---
+++ Module1/factorial.py
@@ -0,0 +1,7 @@
+# -*- encoding: iso-8859-15 -*-
+
+from tkinter import *
+
+def factorielle(event):
+    pass
+
