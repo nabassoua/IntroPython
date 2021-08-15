42959b7c6ca98ad7924619be70d797523193e564
Mohamed Nabassoua <mnabassoua@gmail.com>
2021-08-15 11:57:03 -0400

    New files

---
+++ Module1/dice2.py
@@ -0,0 +1,29 @@
+"""
+Author: Mohamed Nabassoua
+Exercise: using a function, find the number of trials it takes to see a chosen number by the user on the face of a die
+"""
+import random
+
+
+def dice_sim(numtrials):
+
+    num_steps = 1  # Trials counter
+    num_sucess = 0  # Number of trials until sucess
+    print("The random numbers are:", end=' ')
+    while num_steps <= num_trials:
+        random_face = random.randint(1, 6)  # The computer selects a random face between 1 and 6
+        print(random_face, end=' ')
+        num_steps += 1
+        num_sucess += 1
+        if random_face == face_number:
+            print("\n")
+            print("It took", num_sucess, "Trial(s) to get the number", face_number)
+            break
+###############
+# Inputs
+##############
+
+
+num_trials = int(input("Please enter the number of trials: "))
+face_number = int(input("Please choose a face number on the die: "))
+dice_sim(num_trials)
